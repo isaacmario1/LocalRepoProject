660d423b941592dc1f3c9ef486d66c2ececc8159
Isaac Mwangale <isaacmario.m1@gmail.com>
2025-03-21 19:28:27 +0300

    create file 0-iam_betty

---
+++ Desktop/Alx/Control flow/madlibs_generator/madLibGenerator.py
@@ -0,0 +1,10 @@
+print("Welcome to the Mario Mad Libs Game!")
+name = input("Enter name : ")
+place = input("Enter place : ")
+mood = input("Enter mood: ")
+activity = input("Enter activity: ")
+animal = input("Enter animal: ")
+name = input("Enter name: ")
+    
+madlib = f"One day, {name} went to the {place} feeling very {mood}. While there, they decided to spend the day {activity} with a friendly {animal}. By the end of the day, {name} had the most unforgettable adventure!"
+print(madlib)
